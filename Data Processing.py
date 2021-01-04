876a7e1add640accfc6c2568c6d94802fdc6112e
Habib Ullah Khan Jamee <43148756+HabibJamee@users.noreply.github.com>
2021-01-04 23:26:53 +0600

    Create Data Processing.py

---
+++ Data Processing.py
@@ -0,0 +1,12 @@
+#import Libraries
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+get_ipython().run_line_magic('matplotlib', 'inline')
+from sklearn.preprocessing import LabelEncoder
+
+#path derectory
+liver_df=pd.read_csv('')
+
