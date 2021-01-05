939ef17897f2c0a6f62f26dc63c2aca83cc4eac1
Habib Ullah Khan Jamee <43148756+HabibJamee@users.noreply.github.com>
2021-01-05 20:55:39 +0600

    Update Ann_and_Knn.py

--- Ann_and_Knn.py
+++ Ann_and_Knn.py
@@ -3,7 +3,7 @@ from keras.layers import Dense
 import pandas as pd
 import numpy as np
 
-dataset=pd.read_csv('liver.csv')
+dataset=pd.read_csv('')
 dataset.isnull().sum()
 dataset['Albumin_and_Globulin_Ratio'].mean()
 dataset=dataset.fillna(0.94)
@@ -17,10 +17,14 @@ X=dataset[['Age', 'Total_Bilirubin', 'Direct_Bilirubin',
        'Aspartate_Aminotransferase', 'Total_Protiens', 'Albumin',
        'Albumin_and_Globulin_Ratio']]
 y=dataset['Dataset']
+
+#Import Libraries
 from sklearn.model_selection import train_test_split
 
-
+#Train test Split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=123)
+
+#ANN
 
 model=Sequential()
 model.add(Dense(12,input_dim=9,activation='relu'))
@@ -40,3 +44,14 @@ scores2=model.predict(X_test)
 
 print("Testing Accuracy")
 print(model.metrics_names[1],scores2[1]*100)
+
+#KNN
+from sklearn.neighbors import KNeighborsClassifier
+
+KNN = KNeighborsClassifier(n_neighbors=10,p=1)
+KNN.fit(X_train, y_train)
+knn_predicted=KNN.predict(X_test)
+knn_score_train = round(KNN.score(X_train, y_train) * 100, 2)
+knn_score_test = round(KNN.score(X_test, y_test) * 100, 2)
+print('KNN Training Score: \n', knn_score_train)
+print('KNN Test Score: \n', knn_score_test)
